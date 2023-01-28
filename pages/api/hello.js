5ee62389eeeefc298b607cd49b5b9b0fddfdb827
ZahidHasan321 <Dev.zahidhasan@gmail.com>
2023-01-28 19:50:01 +0600

    db connected

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import pool from "@/lib/db";
+
+export default async function handler(req, res) {
+  const result = await pool.query('SELECT * FROM users').then(data => res.send(data.rows));
+   
 }
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import { useEffect } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -121,3 +122,5 @@ export default function Home() {
     </>
   )
 }
+
+
