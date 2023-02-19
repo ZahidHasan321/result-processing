f2fc8f1430d3aef759e01b4ac09df10fc93ea679
ZahidHasan321 <Dev.zahidhasan@gmail.com>
2023-02-19 15:11:05 +0600

    onHold

--- component/dialog/examinerDialog.js
+++ component/dialog/examinerDialog.js
@@ -1,20 +1,36 @@
-import { Dialog, DialogTitle, TextField } from "@mui/material";
-import { useState } from "react";
+import { Box, Dialog, DialogTitle, Grow, TextField } from "@mui/material";
+import { useEffect, useState } from "react";
+import AutoCompleteTeacher from "../selector/autocompleteTeacher";
 
 const ExaminerDialog = (props) => {
-    const {open, onClose} = props;
+    const {open, onClose, semester, session, course} = props;
+
+
 
     const [examinerA, setExaminerA] = useState('')
     const [examinerB, setExaminerB] = useState('')
+    const [List, setList ] = useState([]);
+
+    const getList = async () => {
+        fetch('/api/admin/teacherList')
+          .then(res => res.json())
+          .then(data => {
+            setList(data);
+          });
+      }
+
+    useEffect(() => {
+        getList();
+    },[])
 
     const handleOnClose = () => {
         onClose();
     }
     return (
-        <Dialog open={open} onClose={handleOnClose}>
+        <Dialog  TransitionComponent={Grow} open={open} onClose={handleOnClose}  sx={{ backdropFilter: 'blur(5px)' }}>
             <DialogTitle>Add examiner</DialogTitle>
             <Box component='form'>
-                <TextField /> 
+                <AutoCompleteTeacher list={List} onChange={(value) => setExaminerA(value)}/>
             </Box>
         </Dialog>
     )
