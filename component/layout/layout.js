7440293175333b6c605c0c05dc5813395d66632b
ZahidHasan321 <Dev.zahidhasan@gmail.com>
2023-06-16 04:29:45 +0600

    static title and global store added

--- component/layout/layout.js
+++ component/layout/layout.js
@@ -1,8 +1,13 @@
 import ResponsiveDrawer from "../drawer/persistantDrawer";
+import Head from "next/head";
+
 
 const DrawerLayout = ({ children, showDrawer, ...restProps }) => {
     return (
         <>
+        <Head>
+            <title>Result Processing System</title>
+        </Head>
             <ResponsiveDrawer {...restProps}>
                 {children}
             </ResponsiveDrawer>
---
+++ store/store.js
@@ -0,0 +1,17 @@
+import { create } from 'zustand'
+
+const useDrawerStore = create((set) => ({
+    drawerState: false,
+    toggleDrawer: () => set(state => ({drawerState: !state.drawerState})),
+
+    committeeState: false,
+    toggleCommittee: () => set(state => ({committeeState: !state.committeeState})),
+
+    examinerState: false,
+    toggleExaminer: () => set(state => ({examinerState: !state.examinerState})),
+
+    teacherState: false,
+    toggleTeacher: () => set(state => ({teacherState: !state.teacherState})),
+  }))
+
+export default useDrawerStore;
